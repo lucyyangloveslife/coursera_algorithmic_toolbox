a1d5046e266f24982a9ba5070f1140f3e62f9ecc
lucyyangloveslife <lucyyang@eduroam-int-dhcp-97-60-239.ucl.ac.uk>
2025-05-30 21:06:00 +0100

    comment1

--- Algorithmic Toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
+++ Algorithmic Toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
@@ -3,5 +3,9 @@ def sum_of_two_digits(first_digit, second_digit):
 
 
 if __name__ == '__main__':
+    # input() allows users to type a line of input
+    # split() split the input string into a list of substring based on spaces
+    # map applys int() function to each item in the list
     a, b = map(int, input().split())
+    
     print(sum_of_two_digits(a, b))
