01e6d6a42a83df1ba27c0c8cd273a77e66bb594e
lucyyangloveslife <lucyyang@Lucys-Mac.local>
2025-07-05 21:54:35 +0800

    Update fibonacci_last_digit.py

--- Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -2,12 +2,16 @@ def fibonacci_last_digit(n):
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
+    # Initialize the first two Fibonacci numbers
+    previous = 0 #F(0)
+    current  = 1 #F(1)
 
+    # Compute Fibonacci numbers from F(2) to F(n)
     for _ in range(n - 1):
+        # Update previous and current values for the next step
         previous, current = current, previous + current
 
+    # Return the last digit
     return current % 10
 
 
