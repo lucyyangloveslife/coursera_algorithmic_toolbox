6d1803bfcb694d38247599bd95d55a506838bb9e
Your Name <lucy.yang.life@gmail.com>
2025-07-05 22:55:37 +0800

    update

--- Algorithmic Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,12 +1,18 @@
+# least common multiple
 def lcm(a, b):
+    # loop through numbers from 1 to a * b
+    # a*b is the maximum possible LCM
     for l in range(1, a * b + 1):
+        # check if l is divisable by both a and b
         if l % a == 0 and l % b == 0:
+        # if so, this is the least common multiple
             return l
 
+# this line should never be reached for positive integers
     assert False
 
 
 if __name__ == '__main__':
+    # read two interger from input
     a, b = map(int, input().split())
     print(lcm(a, b))
-
